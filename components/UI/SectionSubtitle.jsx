624617e93774ee580028f50b02986a5fb5ae5e7e
Aditya Yadav <adityabarotha007@gmail.com>
2024-09-30 22:39:06 +0530

    Added the underLine under subtitles

--- components/UI/SectionSubtitle.jsx
+++ components/UI/SectionSubtitle.jsx
@@ -4,7 +4,7 @@ import classes from "../../styles/subtitle.module.css";
 const SectionSubtitle = (props) => {
   return (
     <h5 className={`${classes.section__subtitle}`}>
-      <a href={props.link} target="_blank" rel="noopener noreferrer">
+      <a href={props.link} target="_blank" rel="noopener noreferrer" className="border-b-2 border-black pb-1 inline-block"> 
         {props.subtitle}
       </a>
     </h5>
